cd1b9a751fb065b1c59932c9575ef76f8cb7eb47
rangeswari <arouf21@gmail.com>
2019-02-25 20:25:22 +0530

    dsb

---
+++ src/main/java/com/techprimers/DsbApplication.java
@@ -0,0 +1,14 @@
+package com.techprimers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsbApplication.class, args);
+	}
+
+}
+
